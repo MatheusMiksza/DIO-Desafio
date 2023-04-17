97def9798ae574d2b6c235bd9cb18b028724a155
Miksza <mathmiksza@gmail.com>
2023-04-17 10:38:53 -0300

    Criando Projeto para plugins

---
+++ Lab_final/Lab_Solution/Lab_Solution/Plugins/Instrutores/PreCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace Lab_Solution.Plugins.Instrutores
+{
+    public class PreCreate : PluginBase
+    {
+        public PreCreate(string unsecurem , string secure) : base(typeof(PreCreate)) {
+
+        }
+        protected override void ExecuteCrmPlugin(LocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null) throw new InvalidPluginExecutionException("LocalPluginContext");
+
+            IPluginExecutionContext context = localPluginContext.PluginExecutionContext;
+            IOrganizationService serviceAdmin = localPluginContext.OrganizationServiceAdmin;
+
+            Entity entityTarget = null;
+            if (context.InputParameters.Contains("Target"))
+            {
+                entityTarget = context.InputParameters["Target"] as Entity;
+
+                if (entityTarget == null) return;
+
+                if (!entityTarget.Contains("dio_telefone"))
+                {
+                    throw new InvalidPluginExecutionException("O Telefone é obrigatorio!");
+                }
+            }
+        }
+    }
+}
---
+++ Lab_final/Lab_Solution/Lab_Solution/Util/PluginBase.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Solution
+{
+    public abstract class PluginBase : IPlugin
+    {
+        protected class LocalPluginContext
+        {
+            internal IServiceProvider ServiceProvider { get; private set; }
+
+            internal IOrganizationService OrganizationServiceAdmin { get; private set; }
+            internal IOrganizationService OrganizationService { get; private set; }
+            internal IPluginExecutionContext PluginExecutionContext { get; private set; }
+            internal IServiceEndpointNotificationService NotificationService { get; private set; }
+            internal ITracingService TracingService { get; private set; }
+            internal IOrganizationServiceFactory ServiceFactory { get; private set; }
+
+            private LocalPluginContext() { }
+
+            internal LocalPluginContext(IServiceProvider serviceProvider)
+            {
+                if(serviceProvider == null) throw new ArgumentNullException("serviceProvider");
+
+
+                PluginExecutionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                
+                ServiceProvider = (IServiceProvider)serviceProvider.GetService(typeof(IServiceProvider));
+
+                TracingService = (ITracingService)serviceProvider.GetService(typeof(IServiceProvider));
+
+                NotificationService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
+
+                ServiceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+                OrganizationServiceAdmin = ServiceFactory.CreateOrganizationService(null);
+
+                OrganizationService = ServiceFactory.CreateOrganizationService(PluginExecutionContext.UserId);
+
+            }
+
+            internal void Trace(string message)
+            {
+                if (string.IsNullOrWhiteSpace(message) || TracingService == null)
+                {
+                    return;
+                }
+
+                if (PluginExecutionContext == null)
+                {
+                    TracingService.Trace(message);
+                }
+                else
+                {
+                    TracingService.Trace(
+                        "{0}, Correlation Id: {1}, Initiating User: {2}",
+                        message,
+                        PluginExecutionContext.CorrelationId,
+                        PluginExecutionContext.InitiatingUserId);
+                }
+            }
+
+
+
+
+        }
+
+        //ChildClassName para saber qual clase esta rodando no momento
+        protected string ChildClassName { get; private set; }
+        internal PluginBase(Type childClassName)
+        {
+            ChildClassName = childClassName.ToString();
+        }
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            if(serviceProvider == null) throw new ArgumentNullException("serviceProvider");
+
+            //Chama o LocalPluginContext para desmenbrar o serviceProvider
+            LocalPluginContext localPluginContext = new LocalPluginContext(serviceProvider);
+
+            try {
+
+                //Invoca a implementação
+                ExecuteCrmPlugin(localPluginContext);
+
+                return;
+                
+
+            } catch (FaultException<OrganizationServiceFault> e) {
+
+                localPluginContext.Trace(string.Format(CultureInfo.InvariantCulture, "Exception: {0}", e.ToString()));
+
+            } finally {
+                localPluginContext.Trace(string.Format(CultureInfo.InvariantCulture, "Exiting {0}.Execute()",this.ChildClassName));
+            }
+
+        }
+
+        protected virtual void ExecuteCrmPlugin(LocalPluginContext localcontext)
+        {
+            
+        }
+    }
+}
