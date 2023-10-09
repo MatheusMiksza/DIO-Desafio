fd6da2635c88be98011940ae305d030905da647c
Matheus Miksza <mathmiksza@gmail.com>
2023-10-09 10:50:37 -0300

    Finalizado teste tabela virtual

---
+++ Lab_final/Lab_Solution/Lab_Solution/Models/ConsultaCNPJ.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Solution.Models
+{
+    public class ConsultaCNPJ
+    {
+
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class AtividadePrincipal
+        {
+            [JsonProperty("code", NullValueHandling = NullValueHandling.Ignore)]
+            public string Code;
+
+            [JsonProperty("text", NullValueHandling = NullValueHandling.Ignore)]
+            public string Text;
+        }
+
+        public class AtividadesSecundaria
+        {
+            [JsonProperty("code", NullValueHandling = NullValueHandling.Ignore)]
+            public string Code;
+
+            [JsonProperty("text", NullValueHandling = NullValueHandling.Ignore)]
+            public string Text;
+        }
+
+        public class Billing
+        {
+            [JsonProperty("free", NullValueHandling = NullValueHandling.Ignore)]
+            public bool? Free;
+
+            [JsonProperty("database", NullValueHandling = NullValueHandling.Ignore)]
+            public bool? Database;
+        }
+
+        public class Extra
+        {
+        }
+
+        public class Root
+        {
+            [JsonProperty("abertura", NullValueHandling = NullValueHandling.Ignore)]
+            public string Abertura;
+
+            [JsonProperty("situacao", NullValueHandling = NullValueHandling.Ignore)]
+            public string Situacao;
+
+            [JsonProperty("tipo", NullValueHandling = NullValueHandling.Ignore)]
+            public string Tipo;
+
+            [JsonProperty("nome", NullValueHandling = NullValueHandling.Ignore)]
+            public string Nome;
+
+            [JsonProperty("porte", NullValueHandling = NullValueHandling.Ignore)]
+            public string Porte;
+
+            [JsonProperty("natureza_juridica", NullValueHandling = NullValueHandling.Ignore)]
+            public string NaturezaJuridica;
+
+            [JsonProperty("atividade_principal", NullValueHandling = NullValueHandling.Ignore)]
+            public List<AtividadePrincipal> AtividadePrincipal;
+
+            [JsonProperty("logradouro", NullValueHandling = NullValueHandling.Ignore)]
+            public string Logradouro;
+
+            [JsonProperty("numero", NullValueHandling = NullValueHandling.Ignore)]
+            public string Numero;
+
+            [JsonProperty("municipio", NullValueHandling = NullValueHandling.Ignore)]
+            public string Municipio;
+
+            [JsonProperty("uf", NullValueHandling = NullValueHandling.Ignore)]
+            public string Uf;
+
+            [JsonProperty("data_situacao", NullValueHandling = NullValueHandling.Ignore)]
+            public string DataSituacao;
+
+            [JsonProperty("cnpj", NullValueHandling = NullValueHandling.Ignore)]
+            public string Cnpj;
+
+            [JsonProperty("ultima_atualizacao", NullValueHandling = NullValueHandling.Ignore)]
+            public DateTime? UltimaAtualizacao;
+
+            [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
+            public string Status;
+
+            [JsonProperty("fantasia", NullValueHandling = NullValueHandling.Ignore)]
+            public string Fantasia;
+
+            [JsonProperty("complemento", NullValueHandling = NullValueHandling.Ignore)]
+            public string Complemento;
+
+            [JsonProperty("cep", NullValueHandling = NullValueHandling.Ignore)]
+            public string Cep;
+
+            [JsonProperty("bairro", NullValueHandling = NullValueHandling.Ignore)]
+            public string Bairro;
+
+            [JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
+            public string Email;
+
+            [JsonProperty("telefone", NullValueHandling = NullValueHandling.Ignore)]
+            public string Telefone;
+
+            [JsonProperty("efr", NullValueHandling = NullValueHandling.Ignore)]
+            public string Efr;
+
+            [JsonProperty("motivo_situacao", NullValueHandling = NullValueHandling.Ignore)]
+            public string MotivoSituacao;
+
+            [JsonProperty("situacao_especial", NullValueHandling = NullValueHandling.Ignore)]
+            public string SituacaoEspecial;
+
+            [JsonProperty("data_situacao_especial", NullValueHandling = NullValueHandling.Ignore)]
+            public string DataSituacaoEspecial;
+
+            [JsonProperty("atividades_secundarias", NullValueHandling = NullValueHandling.Ignore)]
+            public List<AtividadesSecundaria> AtividadesSecundarias;
+
+            [JsonProperty("capital_social", NullValueHandling = NullValueHandling.Ignore)]
+            public string CapitalSocial;
+
+            [JsonProperty("qsa", NullValueHandling = NullValueHandling.Ignore)]
+            public List<object> Qsa;
+
+            [JsonProperty("extra", NullValueHandling = NullValueHandling.Ignore)]
+            public Extra Extra;
+
+            [JsonProperty("billing", NullValueHandling = NullValueHandling.Ignore)]
+            public Billing Billing;
+        }
+
+
+
+    }
+}
--- Lab_final/Lab_Solution/Lab_Solution/Plugins/TabelaVirtual/RetrieveMultiplo.cs
+++ Lab_final/Lab_Solution/Lab_Solution/Plugins/TabelaVirtual/RetrieveMultiplo.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Lab_Solution.Models;
+using Newtonsoft.Json;
 
 namespace Lab_Solution.Plugins.TabelaVirtual
 {
@@ -13,14 +18,65 @@ namespace Lab_Solution.Plugins.TabelaVirtual
         {
 
         }
+        ConsultaCNPJ.Root consultaCNPJ = new ConsultaCNPJ.Root();
+        EntityCollection ec = new EntityCollection();
         protected override void ExecuteCrmPlugin(LocalPluginContext localPluginContext)
         {
             if (localPluginContext == null) throw new InvalidPluginExecutionException("LocalPluginContext");
 
             IPluginExecutionContext context = localPluginContext.PluginExecutionContext;
             IOrganizationService serviceAdmin = localPluginContext.OrganizationServiceAdmin;
-
             
+            var qe = (QueryExpression)context.InputParameters["Query"];
+            var a = (string)qe.Criteria.Conditions[0].Values[0];
+            //BuscaCNPJAsync(a);
+            MontaTblVirtual();
+
+            context.OutputParameters["BusinessEntityCollection"] = ec;
+
+
+
+        }
+
+        async Task BuscaCNPJAsync(string cnpj)
+        {
+            string url = $"https://www.receitaws.com.br/v1/cnpj/{cnpj}?callback=?";
+            string retorno = string.Empty;
+            
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    client.Encoding = Encoding.UTF8;
+                    retorno = client.DownloadString(url);
+                    consultaCNPJ =  JsonConvert.DeserializeObject<ConsultaCNPJ.Root>(retorno);
+                }
+
+                
+            }
+            catch (WebException e)
+            {
+                throw new Exception(e.Message);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        void MontaTblVirtual()
+        {
+            
+            Entity entity = new Entity("mm_consultacnpj");
+            entity.Attributes.Add("mm_cnpj", "05.300.584/0001-07");
+            entity.Attributes.Add("mm_name", "Zé das Couve");
+            entity.Attributes.Add("mm_situacao","Plantando Couve");
+
+            ec.Entities.Add(entity);
+
+
         }
     }
 }
